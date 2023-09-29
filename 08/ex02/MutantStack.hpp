8262f6ad6cb966f1902d4a862df361a01ac03836
= <ethandelage04@gmail.com>
2023-09-29 12:27:20 +0200

    Add reverse and const iterator

--- 08/ex02/MutantStack.hpp
+++ 08/ex02/MutantStack.hpp
@@ -19,7 +19,10 @@ template <typename T>
 class MutantStack: public std::stack<T> {
 
 public:
-	typedef typename std::deque<T>::iterator	iterator;
+	typedef typename std::deque<T>::iterator				iterator;
+	typedef typename std::deque<T>::const_iterator			const_iterator;
+	typedef typename std::deque<T>::reverse_iterator		reverse_iterator;
+	typedef typename std::deque<T>::const_reverse_iterator	const_reverse_iterator;
 	MutantStack(): std::stack<T>() {};
 	MutantStack(MutantStack const &other): std::stack<T>(other) {};
 	~MutantStack() {};
@@ -37,6 +40,29 @@ public:
 		return (this->c.begin());
 	}
 
+	const_iterator	cend() {
+		return (this->c.end());
+	}
+
+	const_iterator	cbegin() {
+		return (this->c.begin());
+	}
+
+	reverse_iterator rend() {
+		return (this->c.rend());
+	}
+
+	reverse_iterator	rbegin() {
+		return (this->c.rbegin());
+	}
+
+	const_reverse_iterator crend() {
+		return (this->c.rend());
+	}
+
+	const_reverse_iterator	crbegin() {
+		return (this->c.rbegin());
+	}
 };
 
 #endif
