a1ed6b89cee923c31cef7d7de327383380e03445
= <ethandelage04@gmail.com>
2023-09-29 12:34:24 +0200

    Update main module 08

--- 08/ex02/main.cpp
+++ 08/ex02/main.cpp
@@ -66,9 +66,6 @@ int main() {
 
 		mstack.push(5);
 		mstack.push(17);
-		std::cout << mstack.top() << std::endl;
-		mstack.pop();
-		std::cout << mstack.size() << std::endl;
 		mstack.push(3);
 		mstack.push(5);
 		mstack.push(737);
@@ -82,7 +79,6 @@ int main() {
 			std::cout << *it << std::endl;
 			++it;
 		}
-		std::stack<int> s(mstack);
 	}
 	return 0;
 }
